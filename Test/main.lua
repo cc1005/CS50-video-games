0f5dec26b54f0682f1f028d4c31a5d4ae4c694b3
Conor Campbell <conorkampbell@hotmail.com>
2022-12-27 18:34:58 +0000

    Added paddle movement functionality, and began to add ball movement functionality.

--- Test/main.lua
+++ Test/main.lua
@@ -11,19 +11,36 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 100
+
 function love.load()
     
     love.graphics.setDefaultFilter("nearest", "nearest")
 
+    math.randomseed(os.time())
+
     smallFont = love.graphics.newFont("font.ttf", 8)
 
-    love.graphics.setFont(smallFont)
+    scoreFont = love.graphics.newFont("font.ttf", 24)
     
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT /2 - 2
+
+    ballDX = math.random(2) == 1 and 100 or -100
+    ballDY = math.random(-50, 50)
+
 end
 
 function love.keypressed(key)
@@ -32,16 +49,35 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    if love.keyboard.isDown("w") then
+        player1Y = player1Y + (-PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown("s") then
+        player1Y = player1Y + (PADDLE_SPEED * dt)
+    end
+
+    if love.keyboard.isDown("up") then
+        player2Y = player2Y + (-PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown("down") then
+        player2Y = player2Y + (PADDLE_SPEED * dt)
+    end
+end  
+
 function love.draw()
     push:apply("start")
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
+    love.graphics.setFont(smallFont)
     love.graphics.printf("Hello Pong!", 0, 20, VIRTUAL_WIDTH, "center")
 
-    love.graphics.rectangle("fill", 10, 30, 5, 20)
-    love.graphics.rectangle("fill", VIRTUAL_WIDTH -10, VIRTUAL_HEIGHT -50, 5, 20)
-    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2, 5, 5)
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
+    love.graphics.rectangle("fill", 10, player1Y, 5, 20)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH -10, player2Y, 5, 20)
+    love.graphics.rectangle("fill", VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2, 4, 4)
 
     push:apply("end")
 end 
