6e34c2c4ba9b71f6a34fa336429825e3b9fe6315
Conor Campbell <conorkampbell@hotmail.com>
2022-12-28 14:43:01 +0000

    All classes plugged into ther main.lua file, and a new class.lua file added to allow for easier use of classes in lua.

--- Test/Ball.lua
+++ Test/Ball.lua
@@ -13,7 +13,7 @@ end
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_WIDTH / 2 - 2
-    self.dy = math.random(2) == 1 and =100 or 100
+    self.dy = math.random(2) == 1 and -100 or 100
     self.dx = math.random(-50, 50)
 end
 
