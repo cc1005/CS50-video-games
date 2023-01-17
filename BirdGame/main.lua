47b2cd6365af08f2a625fedbd0ca63e7acf87d49
Conor Campbell <conorcampbell@live.com>
2023-01-17 21:02:12 +0000

    Implemented looping in the parallax scrolling
    Used modulo functionality to allow for the background and the ground to loop, creating a parallax effect.

--- BirdGame/main.lua
+++ BirdGame/main.lua
@@ -10,8 +10,10 @@ local background = love.graphics.newImage('background.png')
 local ground = love.graphics.newImage('ground.png')
 local backgroundScroll = 0
 local groundScroll = 0
-local BACKGROUND_SCROLL_SPEED = 10
-local GROUND_SCROLL_SPEED = 30
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -43,6 +45,9 @@ function love.draw()
 end
 
 function love.update(dt)
-    backgroundScroll = backgroundScroll + BACKGROUND_SCROLL_SPEED
-    groundScroll = groundScroll + GROUND_SCROLL_SPEED
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % VIRTUAL_WIDTH
 end
