4f693e5a3b1f43c5b1f88a0d9e9b70287cca31c8
Conor Campbell <conorcampbell@live.com>
2023-01-19 23:13:03 +0000

    Finished pipe pair logic
    But heavily indebted to the course, didn't really go off script here. Come back and comment this to make sure I fully understand it, and try to modify it in some way.

--- BirdGame/Pipe.lua
+++ BirdGame/Pipe.lua
@@ -1,19 +1,27 @@
 Pipe = Class{}
 
 local PIPE_IMAGE = love.graphics.newImage('Assets/pipe.png')
+ 
+PIPE_SPEED = 40
 
-local PIPE_SCROLL = -40
+PIPE_HEIGHT = 288
+PIPE_WIDTH = 70
 
-function Pipe:init()
-    self.x = VIRTUAL_WIDTH + 20
-    self.y = math.random(VIRTUAL_HEIGHT /4, VIRTUAL_HEIGHT - 10)
+function Pipe:init(orientation, y)
+    self.x = VIRTUAL_WIDTH
+    self.y = y 
+
     self.width = PIPE_IMAGE:getWidth()
+    self.height = PIPE_HEIGHT
+
+    self.orientation = orientation
 end
 
 function Pipe:update(dt)
-    self.x = self.x + PIPE_SCROLL * dt
+    self.x = self.x + PIPE_SPEED * dt
 end
 
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, self.x, self.y)
+    love.graphics.draw(PIPE_IMAGE, self.x, 
+        (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y), 0, 1, self.orientation == 'top' and -1 or 1)
 end
