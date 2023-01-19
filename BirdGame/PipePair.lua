2593e75c4c5726643d3e0feee3fd1b2f7ce21386
Conor Campbell <conorcampbell@live.com>
2023-01-19 23:05:03 +0000

    Creating pipe pairs logic.

--- BirdGame/PipePair.lua
+++ BirdGame/PipePair.lua
@@ -1,8 +1,8 @@
-PipePair = Clas{}
+PipePair = Class{}
 
 local GAP_HEIGHT = 120
 
-function PipePaid:init(y)
+function PipePair:init(y)
     self.x = VIRTUAL_WIDTH + 32
     self.y = y 
     self.pipes = {
@@ -14,5 +14,17 @@ function PipePaid:init(y)
 end
 
 function PipePair:update(dt)
-    
+    if self.x > -PIPE_WIDTH then
+        self.x = self.x - PIPE_SPEED * dt
+        self.pipes['lower'].x = self.x
+        self.pipes['upper'].x = self.x 
+    else
+        self.remove = true
+    end
+end
+
+function PipePair:render()
+    for k, pipe in pairs(self.pipes) do
+        pipe:render()
+    end
 end
