f48165665068c35c0bff2ca88e18712e1aa19930
Conor Campbell <conorcampbell@live.com>
2023-01-22 18:54:33 +0000

    Populating the basic state classes
    As I understand it, BaseState.lua is allowing all other states to inherit this functionality to these functions can always be called, even if they have nothing to do when called (so as not to throw an error)

---
+++ BirdGame/states/TitleScreenState.lua
@@ -0,0 +1,15 @@
+TitleScreenState = Class{_includes = BaseState}
+
+function TitleScreenState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('retiurn') then
+        gStateMachine:change('play')
+    end
+end
+
+function TitleScreenState:render()
+    love.graphics.setFont(largeFont)
+    love.graphics.printf('Bird Game!', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter!', 0, 100, VIRTUAL_WIDTH, 'center')
+end
